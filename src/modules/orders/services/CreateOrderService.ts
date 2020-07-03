64dc91f80b3bc813ad7aebcca5e7de144748ca7d
Mateus Bueno Santos <mateusbueno382@gmail.com>
2020-07-03 02:50:32 -0300

    final result with all tests passed

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,62 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('User does not exist');
+    }
+
+    const productIds = products.map(product => ({ id: product.id }));
+
+    const foundProducts = await this.productsRepository.findAllById(productIds);
+
+    if (foundProducts.length !== productIds.length) {
+      throw new AppError('Invalid product');
+    }
+
+    const productsToBeUpdated = foundProducts.map(foundProduct => {
+      const product = products.find(p => p.id === foundProduct.id);
+
+      if (!product) {
+        throw new AppError('Invalid Product');
+      }
+
+      if (product.quantity > foundProduct.quantity) {
+        throw new AppError(
+          `Product ${foundProduct.name} with insuficient quantity`,
+        );
+      }
+
+      return {
+        ...foundProduct,
+        quantity: foundProduct.quantity - product.quantity,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: foundProducts.map(product => ({
+        product_id: product.id,
+        price: product.price,
+        quantity: products.find(p => p.id === product.id)?.quantity ?? 0,
+      })),
+    });
+
+    await this.productsRepository.updateQuantity(productsToBeUpdated);
+
+    return order;
   }
 }
 
